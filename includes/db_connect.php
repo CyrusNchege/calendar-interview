c18373f993b59e2a3fd14a5dd2880254111071ec
cyrusnchege <cyruschegecc@gmail.com>
2024-08-20 07:32:31 +0300

    Add db connection and initial table schema

---
+++ includes/db_connect.php
@@ -0,0 +1,20 @@
+<?php
+
+$servername = "localhost";
+$username = "cyrus";
+$password = "cyrus123";
+$dbname = "calendar_db";
+
+
+
+// Create connection
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully"; 
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
